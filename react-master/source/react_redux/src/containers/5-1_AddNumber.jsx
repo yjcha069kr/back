a013a9ef8b2980f5a48e0968dbff3c3fb8372dbf
yjcha069kr <yjcha069kr@gmail.com>
2025-12-09 09:39:45 +0900

    처음

--- react-master/source/react_redux/src/containers/5-1_AddNumber.jsx
+++
@@ -1,7 +0,0 @@
-import AddNumber from "../components/AddNumber";
-import React, { Component } from "react";
-export default class extends Component {
-  render() {
-    return <AddNumber></AddNumber>
-  }
-} 
--- vite/my-app/src/App.jsx
+++
@@ -1,7 +0,0 @@
-function App() {
-  return (
-    <h1>Hello, Vite!</h1>
-  )
-}
-
-export default App
